3d70276f8008c339f7a04c3ac49a077d199e4ebf
Jackey.Sparrow <1109952026@qq.com>
2016-04-17 12:01:59 +0800

    add login page

---
+++ app/pages/login/login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jackey Li on 2016/4/17.
+ */
+import {Page} from 'ionic-angular';
+
+@Page({
+    template: 'build/login/login.html'
+})
+
+export class Login {
+    constructor() {
+
+    }
+}
