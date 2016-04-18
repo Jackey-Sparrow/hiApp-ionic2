27270b57973185e0cc605b0957f95352dbf478e8
Jackey.Sparrow <1109952026@qq.com>
2016-04-18 21:01:51 +0800

    add loading

--- app/pages/tweet/tweet.js
+++ app/pages/tweet/tweet.js
@@ -1,20 +1,54 @@
-import {Page, Http} from 'ionic-angular';
+import {Page, Loading, NavController} from 'ionic-angular';
+import {Http} from 'angular2/http';
 
 
 @Page({
-  templateUrl: 'build/pages/tweet/tweet.html'
+    templateUrl: 'build/pages/tweet/tweet.html'
 })
 export class Tweet {
 
-  static get parameters() {
-    return [[Http]];
-  }
+    static get parameters() {
+        return [[Http], [NavController]];
+    }
 
-  constructor(http) {
-    this.http = http;
+    constructor(http, nav) {
+        this.http = http;
+        this.nav = nav;
+        this.loading;
+        this.tweets = [];
 
-    this.http.get('data/comments.json').toRx().subscribe((res:Response) => {
-      this.status = res.status;
-    });
-  }
+        //todo: not well, may use onPageLoaded
+        setTimeout(()=> {
+            this.loadTweet();
+        }, 500);
+    }
+
+    loadTweet() {
+        this.presentLoading();
+        this.http.get('data/comments.json').subscribe(res => {
+            console.log(res.json());
+            setTimeout(()=> {
+                this.closeLoading();
+                this.tweets = res.json();
+            }, 2000);
+        });
+    }
+
+    presentLoading() {
+        this.loading = Loading.create({
+            content: 'Loading...',
+            duration: 3000,
+            dismissOnPageChange: true
+        });
+
+        this.nav.present(this.loading);
+    }
+
+    closeLoading() {
+        this.loading.dismiss();
+    }
+
+    //onPageLoaded(){
+    //    this.loadTweet();
+    //}
 }
