8170ff74d25505149d0ab25b8ec9967406551244
Jackey.Sparrow <1109952026@qq.com>
2016-04-27 20:01:49 +0800

    implement load data

--- app/pages/contacts/services/contact-service.js
+++ app/pages/contacts/services/contact-service.js
@@ -1,3 +1,27 @@
 /**
  * Created by Jackey Li on 2016/4/26.
  */
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+
+export class ContactService {
+
+    //todo: not working
+    //static get parameters() {
+    //	return [[Http]];
+    //}
+
+    constructor(http) {
+        this.http = http;
+    }
+
+    loadData(http) {
+        let that = this;
+        var promise = new Promise(function (resolve, reject) {
+            that.http.get('data/contacts.json').subscribe(res => {
+                resolve(res.json());
+            });
+        });
+
+        return promise;
+    }
+}
