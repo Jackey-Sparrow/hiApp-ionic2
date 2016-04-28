0e243478c229858117e9015de98657e3ebebac2c
Jackey.Sparrow <1109952026@qq.com>
2016-04-28 20:04:45 +0800

    use service

--- app/pages/contacts/contacts.js
+++ app/pages/contacts/contacts.js
@@ -1,4 +1,6 @@
 import {Page, Translate} from 'ionic-angular';
+import {ContactService} from './services/contact-service.js';
+import {Http} from 'angular2/http';
 
 @Page({
     templateUrl: 'build/pages/contacts/contacts.html'
@@ -7,12 +9,15 @@ import {Page, Translate} from 'ionic-angular';
 export class Contacts {
 
     static get parameters() {
-        return [[Translate]];
+        return [[Translate], [Http]];
     }
 
-    constructor(translate) {
+    constructor(translate, http) {
         this.translate = translate;
+        this.http = http;
 
         this.title = this.translate.translate('contactTitle');
+
+        this.dataService = new ContactService(this.http);
     }
 }
